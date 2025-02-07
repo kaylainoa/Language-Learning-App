1c42e0cd33f001589db0411dfb16dba20dcfddef
kaylainoa <kinoa718@gmail.com>
2025-02-07 17:05:46 -0500

    Simple documentation

--- wicse-language/src/App.js
+++ wicse-language/src/App.js
@@ -1,3 +1,14 @@
+/*
+
+WICSE React project led by Kayla Inoa, built by Daya, Diya, Paulina, and Abigail.
+
+Uses React, React Router, and Tailwind for a multi-page learning app.
+
+GitHub: https://github.com/kaylainoa/Language-Learning-App
+
+*/
+
+
 import logo from './logo.svg';
 import './App.css';
 
