530ba1c781c33d41056dd33a76256b0e8321bed2
Diya Raj <diya.r71414@gmail.com>
2025-02-23 18:53:38 -0500

    test

--- wicse-language/src/beachball.js
+++ wicse-language/src/beachball.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function beachball() {
+function BeachBall() {
   return (
     <div>
-      <h1>Beachball</h1>
+      <h1>Beach Ball Game</h1>
     </div>
   );
 }
 
-export default beachball;
+export default BeachBall;
