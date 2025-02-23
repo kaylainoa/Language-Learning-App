6e450a619f7830c76efc27e86005829ccf642b14
kaylainoa <kinoa718@gmail.com>
2025-02-23 18:25:22 -0500

    test

--- wicse-language/src/beachball.js
+++ wicse-language/src/beachball.js
@@ -1,1 +1,11 @@
-//File for Beach Ball Mini game
+import React from 'react';
+
+function BeachBall() {
+  return (
+    <div>
+      <h1>Beachball</h1>
+    </div>
+  );
+}
+
+export default Coconut;
