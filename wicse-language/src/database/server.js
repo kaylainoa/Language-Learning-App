93996eab6beb46a8c01198bbb95644aa9b2617b6
abigailere <100704640+abigailere@users.noreply.github.com>
2025-03-11 20:15:51 -0400

    connected to mongdb

--- wicse-language/src/database/server.js
+++ wicse-language/src/database/server.js
@@ -1,8 +1,11 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const Product = require ('./models/product.model');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
+app.use(express.json());
 
 
 const uri = "mongodb+srv://abigailerefah:<password>@user-authentication.atn31.mongodb.net/?retryWrites=true&w=majority&appName=User-Authentication";
@@ -13,16 +16,21 @@ app.get('/', (req,res) => {
   res.send("hello from node");
 });
 
-app.post('/api/products', (req, res) => {
-  req.body
-  res.send("data recieved");
+
+app.post('/api/products', async(req, res) => {//used to save something
+  try{
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  }catch (error) {
+    res.status(500).json({message: error.message});
+  }
 });
 
 mongoose.connect(uri)
 .then(() => {
-  console.log("connected to db");
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("connected to db", mongoose.connection.name);
+  app.listen(3001, () => {
+    console.log('Server is running on port 3001');
   });
 })
 .catch((error) => {
