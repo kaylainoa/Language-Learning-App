e4fe219a5ed2b8589b228ce2ef9e6507272b9dbe
abigailere <100704640+abigailere@users.noreply.github.com>
2025-03-25 02:02:25 -0400

    updated files to show question and answer slot

--- wicse-language/src/Components/Coconut.js
+++ wicse-language/src/Components/Coconut.js
@@ -3,6 +3,9 @@ import './Coconut.css';
 import { useState, useEffect } from 'react';
 
 function Coconut() {
+  const [typedLetters, setTypedLetters] = useState([]); // State to hold typed letters
+  const wordLength = 4;
+
   return (
     <div className="coconut-background">
       <div className="question-container">
@@ -10,6 +13,12 @@ function Coconut() {
         <div className="word-container">
           <span className="word">to read</span>
         </div>
+      </div>
+      <div className="typed-letters-box">
+        {/* Render empty bars */}
+        {Array.from({ length: wordLength }).map((_, index) => (
+          <div key={index} className="letter-slot"></div>
+        ))}
       </div>
     </div>
   );
