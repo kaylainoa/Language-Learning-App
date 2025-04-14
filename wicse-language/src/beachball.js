cf642f6b9012e237e81521f27cf020dd6762a49d
pgonzalezbrito26 <pgonzalezbrito@ufl.edu>
2025-04-14 15:46:29 -0400

    coral updates

--- wicse-language/src/beachball.js
+++ wicse-language/src/beachball.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-//File for Beach Ball Mini game
-=======
 import React from 'react';
 
 function BeachBall() {
@@ -12,4 +9,3 @@ function BeachBall() {
 }
 
 export default BeachBall;
->>>>>>> 530ba1c781c33d41056dd33a76256b0e8321bed2
